873f8b5e7a1d41356d97f3bfaf9ca1aeedccb35c
SimonasBubnysUM <71946857+SimonasB333@users.noreply.github.com>
2021-10-11 23:05:36 +0200

    a

---
+++ src/main/java/com/pizza/application/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.pizza.application.repository;
+
+import com.pizza.application.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
